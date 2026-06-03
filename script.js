e8216dec9590a4ea1fc22ca787cda29759f16268
ikimodus <ifhumail@gmail.com>
2026-06-03 23:26:27 +0200

    Custom Grid Size Btn

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 const grid = document.querySelector(".grid");
-const gridPixel = document.querySelector(".grid-pixel");
+const buttonGridSize = document.querySelector(".input-grid-size");
 
 function createGrid(squaresPerSide) {
   let GridSize = squaresPerSide * squaresPerSide;
   let pixelPercentage = 100 / squaresPerSide;
 
-  console.log(pixelPercentage);
-  gridPixel.style.width = pixelPercentage + "%";
-  for (let i = 0; i < GridSize - 1; i++) {
-    const clonedPixel = gridPixel.cloneNode(true);
-    grid.appendChild(clonedPixel);
-    console.log(i);
+  grid.innerHTML = "";
+  for (let i = 0; i < GridSize; i++) {
+    const gridPixel = document.createElement("div");
+    gridPixel.setAttribute("class", "grid-pixel");
+    gridPixel.style.width = pixelPercentage + "%";
+
+    grid.appendChild(gridPixel);
   }
+
+  document
+    .querySelectorAll(".grid-pixel")
+    .forEach((element) =>
+      element.addEventListener(
+        "mouseover",
+        () => (element.style.backgroundColor = "pink"),
+      ),
+    );
 }
+
+function gridSizeInput() {
+  let gridSizeInput = prompt("Type Grid Size");
+  if (isNaN(gridSizeInput)) return;
+  else if (gridSizeInput > 100) gridSizeInput = 100;
+
+  createGrid(gridSizeInput);
+}
+
 createGrid(16);
 
-document
-  .querySelectorAll(".grid-pixel")
-  .forEach((element) =>
-    element.addEventListener(
-      "mouseover",
-      () => (element.style.backgroundColor = "pink"),
-    ),
-  );
+buttonGridSize.addEventListener("click", gridSizeInput);
