a0e23aaf9e9edf83a4dd725183543bc01f823dc0
ikimodus <ifhumail@gmail.com>
2026-06-03 22:35:38 +0200

    HTML, CSS add + Grid Creation

---
+++ script.js
@@ -0,0 +1,16 @@
+const grid = document.querySelector(".grid");
+const gridPixel = document.querySelector(".grid-pixel");
+
+function createGrid(squaresPerSide) {
+  let GridSize = squaresPerSide * squaresPerSide;
+  let pixelPercentage = 100 / squaresPerSide;
+
+  console.log(pixelPercentage);
+  gridPixel.style.width = pixelPercentage + "%";
+  for (let i = 0; i < GridSize - 1; i++) {
+    const clonedPixel = gridPixel.cloneNode(true);
+    grid.appendChild(clonedPixel);
+    console.log(i);
+  }
+}
+createGrid(16);
